72edb5770a0b8eaf963a282be34adcd5f1064e1d
Melis Sert <melisrt8@gmail.com>
2025-11-12 18:37:28 +0000

    Added OPTIONS handling

--- backend/main.go
+++ backend/main.go
@@ -19,6 +19,13 @@ func main() {
 
 func ToDoListHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 
 	//Handle GET request: return all todos
 	if r.Method == http.MethodGet {
